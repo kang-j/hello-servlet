d7167e80df6bd9f5ed0dac81db86e6a064b24ec4
juhyung-kang@taiyo-st.co.jp <juhyung-kang@taiyo-st.co.jp>
2019-10-17 18:54:29 +0900

    test

--- src/test/java/com/example/hello/HelloServletTest.java
+++ src/test/java/com/example/hello/HelloServletTest.java
@@ -27,7 +27,7 @@ public class HelloServletTest {
 
 
 		assertThat(res.getContentType(), equalTo("text/plain;charset=utf-8"));
-		assertThat(writer.toString(), equalTo("Hello World V04!!!"));
+		assertThat(writer.toString(), equalTo("Hello World V01!!!"));
 	}
 
 	static class MockHttpServletResponse implements HttpServletResponse {
